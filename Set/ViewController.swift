4531ef43fd1cede80a1b1fd47bb0543809bdde1a
Vladimir Fibe <motiw@icloud.com>
2023-08-06 21:51:25 +0600

    feat: add and show CardView

--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -1,19 +1,27 @@
-//
-//  ViewController.swift
-//  Set
-//
-//  Created by Vladimir Fibe on 06.08.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    let cardView = CardView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
+    }
+}
+// MARK: - Setup Views
+extension ViewController {
+    private func setupViews() {
+        view.backgroundColor = .black
+        setupCardView()
     }
 
-
+    private func setupCardView() {
+        view.addSubview(cardView)
+        cardView.frame = CGRect(x: 50, y: 100, width: 200, height: 300)
+        cardView.backgroundColor = .white
+        cardView.layer.cornerRadius = 20
+        cardView.layer.masksToBounds = true
+    }
 }
 
